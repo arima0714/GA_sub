5873e3e46fb33ca654a1c877bed09ba86685e72b
arima0714 <a1710021@edu.cc.uec.ac.jp>
2019-11-07 11:29:11 +0900

    gaで必要となる配列群を宣言した

---
+++ ga_sub.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define NUM_OF_GENE 4	// 生存可能な数の上限
+#define NUM_OF_CHROMOSOME 6	// 各遺伝子が保有する染色体の数 // 並び替える値の個数
+
+int** index_1[NUM_OF_GENE];
+int** index_2[NUM_OF_GENE];
+
+int* array_1[NUM_OF_CHROMOSOME];
+int* array_2[NUM_OF_CHROMOSOME];
+int* array_3[NUM_OF_CHROMOSOME];
+int* array_4[NUM_OF_CHROMOSOME];
+
+int* root_array[NUM_OF_CHROMOSOME];	// ソートしたい配列
+
+int main(void){
+
+	return 0;
+}
+
