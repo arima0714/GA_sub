d120bf419723266527b42b893b198d6fb22e8be8
arima0714 <a1710021@edu.cc.uec.ac.jp>
2019-11-11 22:52:33 +0900

    適応度用の枠を追加した

--- ga_sub.c
+++ ga_sub.c
@@ -6,13 +6,13 @@
 #define NUM_OF_GENE 4	// 生存可能な数の上限
 #define NUM_OF_CHROMOSOME 6	// 各遺伝子が保有する染色体の数 // 並び替える値の個数
 
-int index_1[NUM_OF_GENE][NUM_OF_CHROMOSOME];
-int index_2[NUM_OF_GENE][NUM_OF_CHROMOSOME];
+int index_1[NUM_OF_GENE][NUM_OF_CHROMOSOME +1];
+int index_2[NUM_OF_GENE][NUM_OF_CHROMOSOME +1];
 
-int array_1[NUM_OF_CHROMOSOME];
-int array_2[NUM_OF_CHROMOSOME];
-int array_3[NUM_OF_CHROMOSOME];
-int array_4[NUM_OF_CHROMOSOME];
+int array_1[NUM_OF_CHROMOSOME +1];
+int array_2[NUM_OF_CHROMOSOME +1];
+int array_3[NUM_OF_CHROMOSOME +1];
+int array_4[NUM_OF_CHROMOSOME +1];
 
 int root_array[NUM_OF_CHROMOSOME];	// ソートしたい配列
 
