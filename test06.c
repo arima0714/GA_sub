dd987c648c38cfc148a06471cab5ca3e0298cae5
arima0714 <a1710021@edu.cc.uec.ac.jp>
2019-12-06 11:40:19 +0900

    修正したランダム関数を作成した

--- test06.c
+++ test06.c
@@ -14,7 +14,10 @@ int return_rand(int num){
 int return_rand_fixed(int num){
 	// 実行時期が限りなく近いときに返値がバラバラにする必要がある
 	int s;
-	srand(time(NULL));
+	struct timeval myTime;
+	struct tm *time_st;
+	time_st = localtime(&myTime.tv_sec);
+	srand(time(NULL) + myTime.tv_usec);
 	s = rand() % num + 1;
 	return s;
 }
