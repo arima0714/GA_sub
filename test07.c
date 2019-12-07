8c58b6c2b56e878858ff3eff85d8914fc1517b22
arima0714 <a1710021@edu.uec.ac.jp>
2019-12-07 22:35:20 +0900

    ランダムのテストをした

---
+++ test07.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void){
+	int s;
+		srand(time(NULL));
+		s = rand() % 10 +1;
+		printf("%d\n",s);
+		srand(time(NULL));
+		s = rand() % 10 +1;
+		printf("%d\n",s);
+		srand(time(NULL));
+		s = rand() % 10 +1;
+		printf("%d\n",s);
+	return 0;
+}
