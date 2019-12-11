09bd9c259c9d4508608618a2288d777b33856f62
arima0714 <a1710021@edu.cc.uec.ac.jp>
2019-12-11 15:22:50 +0900

    init_indexed()のテストを追加した

--- test.c
+++ test.c
@@ -1,2 +1,21 @@
+
 #include "test.h"
 #include "ga_sub.h"
+#include <stdio.h>
+
+void init_indexes_test(){
+    int NOG = 4;
+    int NOC = 6;
+    int test_index[NOG][NOC];
+    int test_root_array[NOC];
+
+    init_indexes(NOG, NOC, test_index, test_root_array);
+
+    for(int i = 0; i < NOG; i++){
+        for(int j = 0; j < NOC; j++){
+            if(test_index[NOG][NOC] != NOC -j){
+                fprintf(stderr, "crossing_test() is failed \n");
+            }
+        }
+    }
+}
